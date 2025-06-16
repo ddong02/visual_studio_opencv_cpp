20d1ebed28bf727f3926456c2884b28f323e46b9
eogus <kddhh2021@gmail.com>
2025-06-16 21:06:35 +0900

    chapter 8-2 quiz 5

--- Project_opencv/main.cpp
+++ Project_opencv/main.cpp
@@ -14,6 +14,8 @@ int main(void)
 	int key;
 	bool normal_mode = true;
 	Mat M;
+	Mat zoomout_center;
+	bool zoomout_mode = false;
 
 	while (true) {
 		cap >> frame;
@@ -23,19 +25,29 @@ int main(void)
 		}
 		if (!normal_mode) {
 			warpAffine(frame, frame, M, Size());
+			if (zoomout_mode) {
+				zoomout_center = Mat_<double>({ 2,3 }, { 1,0,frame.cols / 4.0,0,1,frame.rows / 4.0 });
+				warpAffine(frame, frame, zoomout_center, Size());
+			}
 		}
 		imshow("Frame", frame);
 		key = waitKey(10);
 		if (key == 'q') break;
 		else if (key == 'a' || key == 'b') {
 			normal_mode = false;
-			if (key == 'a')
+			if (key == 'a') {
 				M = Mat_<double>({ 2,3 }, { 2.0, 0,0,0,2.0,0 });
-			else
+				zoomout_mode = false;
+			}
+			else {
+				zoomout_mode = true;
 				M = Mat_<double>({ 2,3 }, { 0.5, 0,0,0,0.5,0 });
+			}
 		}
-		else if (key == 'c')
-			normal_mode = true;;
+		else if (key == 'c') {
+			zoomout_mode = false;
+			normal_mode = true;
+		}
 	}
 
 	return 0;
